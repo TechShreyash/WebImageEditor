e4c16cc7bdca4c14b4742a71709f9017755ce360
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2024-02-21 20:38:33 +0530

    Update resizeHandler.py

--- Api/resizeHandler.py
+++ Api/resizeHandler.py
@@ -8,6 +8,6 @@ def resizeImage(max_height, max_width, hash):
     width, height = img.size
     resize_ratio = min(max_width / width, max_height / height)
     new_width, new_height = int(width * resize_ratio), int(height * resize_ratio)
-    img = img.resize((int(new_width), int(new_width)))
+    img = img.resize((int(new_width), int(new_height)))
     img.save(f"/tmp/{hash}")
     return hash
