f4c17b9db2203ba31403471b6fc12dcd31f383f0
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2024-02-21 18:37:08 +0530

    up

--- Api/app.py
+++ Api/app.py
@@ -28,8 +28,8 @@ def resize_image():
     width = request.args.get("width")
     height = request.args.get("height")
     fileUrl = f"{url}/uploads/{file}"
+
     image = resizeImage(fileUrl, height, width, file)
-    return Response(image, content_type="image/jpeg", status=200)
     imageUrl = f"{url}/uploads/{image}"
     return imageUrl
 
--- Api/resizeHandler.py
+++ Api/resizeHandler.py
@@ -5,12 +5,12 @@ def resizeImage(fileUrl, height, width, hash):
     url = f"https://wsrv.nl/?url={fileUrl}&w={width}&h={height}"
     response = requests.get(url)
     file = response.content
-    return file
 
     with open(f"/tmp/resized{hash}", "wb") as f:
         f.write(file)
-    
+
     return f"resized{hash}"
+
 
 # a = resizeImage('https://image-editor-api.vercel.app/uploads/EdO1dCSJlj.jpg',600,600,'EdO1dCSJlj.jpg')
 # with open('resizedEdO1dCSJlj.jpg','wb') as f:
